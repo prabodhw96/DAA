a81ad25be934b4207d11b3c9ca0349e1a5630654
prabodhw96 <prabodhwankhede1996@gmail.com>
2018-06-22 23:19:54 +0530

    Rabin-Karp

---
+++ Rabin-Karp.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+// d is the number of characters in the input alphabet
+#define d 256
+void search(string pat, string txt, int q)
+{
+    int M = pat.length();
+    int N = txt.length();
+    int i, j;
+    int p = 0;
+    int t = 0;
+    int h = 1;
+    for(i=0; i<M-1; i++)
+        h = (h*d)%q;
+    for(i=0; i<M; i++)
+    {
+        p = (d*p + pat[i])%q;
+        t = (d*t + txt[i])%q;
+    }
+    for(i=0; i<=N-M; i++)
+    {
+        if(p == t)
+        {
+            for(j=0; j<M; j++)
+            {
+                if(txt[i+j] != pat[j])
+                    break;
+            }
+            if(j == M)
+                cout<<"Pattern found at index "<<i<<endl;
+        }
+        if(i<N-M)
+        {
+            t = (d*(t-txt[i]*h)+txt[i+M])%q;
+            if(t<0)
+                t += q;
+        }
+    }
+}
+int main()
+{
+    string txt = "AN EYE FOR AN EYE";
+    string pat = "EYE";
+    int q = 101; // A prime number
+    search(pat, txt, q);
+    return 0;
+}
